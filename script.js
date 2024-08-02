66fc1aeaa5d87c89330c4182a082067496a4087a
Gabriel Nathaniel <106745929+Natigabriel@users.noreply.github.com>
2024-08-01 20:55:08 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-// Guess the Number Game
-// Description: Create a game where the user has to guess a randomly generated number within a certain range.
-
 const input = document.querySelector('input');
+const btn = document.querySelector('button');
+// const guessList = document.createElement('ul');
 
-const click = () => {
-    const randomNumber = Math.floor(Math.random() * 50) + 1;
-    const answer = parseInt(input.value); // Assuming 'input' is defined elsewhere
-    
-    const newParagraph = document.createElement('p');
-    
-   
-    
-    document.body.appendChild(newParagraph); // Assuming you want to append it to the body
-}
+// document.body.appendChild(guessList);
 
+let numberOfGuesses = 0;
+let guessedNums = [];
+
+btn.addEventListener('click', () => {
+    const num = input.value;
+    const randomNum = Math.floor(Math.random() * 50) + 1;
+
+    numberOfGuesses++;
+    guessedNums.push(num);
+    
+
+    // guessList.innerHTML = ''; // Clear the previous list
+    // guessedNums.forEach(value => {
+    //     const listItem = document.createElement('li');
+    //     listItem.textContent = value;
+    //     guessList.appendChild(listItem);
+    // });
+
+    if (num === randomNum) {
+        document.getElementById('firstp').textContent = `You guessed it right!`;
+    } else if (num > randomNum) {
+        document.getElementById('firstp').textContent = `Your guess was too high!`;
+    } else {
+        document.getElementById('firstp').textContent = `Your guess was too low!`;
+    }
+    document.getElementById('secondp').textContent = `No. of guesses: ${numberOfGuesses}`;
+    document.getElementById('thirdp').textContent = `You've guessed: ${guessedNums.join(', ')}`;
+});
